db8465dbdeec77744bf4e6a99c0476a7ad183aa1
Christopher Pearce <christopher.pearce@outlook.co.nz>
2018-08-19 21:08:36 +1200

    scraped data and adjusting as such

--- scraper/createnames.R
+++ scraper/createnames.R
@@ -2,9 +2,36 @@ library(tidyverse)
 library(data.table)
 
 babynames = fread("babynames.csv") %>%
-  select(-url, -scrape.url,-Letter) %>%
-  filter(Name != "")
+  select(-url,-scrape.url, -Letter) %>%
+  filter(Name != "") %>% unique()
+
+
+directories = babynames %>%
+  select(category,
+         Gender) %>%
+  unique()
+
+
+lapply(1:nrow(directories),
+       function(x) {
+         get.dir = directories[x,]
+         
+         retter = babynames %>%
+           filter(category == get.dir$category,
+                  Gender == get.dir$Gender) %>%
+             select(Name)
+         
+         
+         fwrite(retter,
+                     paste("data",
+                            get.dir$Gender,
+                            paste0(get.dir$category,".txt")
+                           ,sep="/"),
+                quote=FALSE,
+                col.names = FALSE)
+         
+         1
+       })
 
 
 
-chainer = matrix(0, nrow = 26, ncol = max(nchar(babynames$Name)))
